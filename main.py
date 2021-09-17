b026cc77e93d18c6c42dbca46edbb67a1961773d
SuliBalazs <suli.bali01@hotmail.com>
2021-09-17 15:27:06 +0200

    asd

---
+++ main.py
@@ -0,0 +1,23 @@
+kiscica=5
+print(kiscica + 5)
+print("kiscica")
+
+width, lenght = 4 , 5 # m
+area = width*lenght
+print("szoba területe", area , "m^2.")
+lenght2 = 2 #m
+area=width*lenght2
+print("az új szoba hossza: ", area , "M^2")
+width3= lenght3 = 5
+print("az új szoba hossza: ", width3*lenght3 , "M^2")
+
+nap=7
+ora=24
+ora2=26
+perc=60
+ido=0
+
+ido=(nap*ora)*perc
+print("ennyi perc van egy héten:", ido)
+ido=(nap*ora2)*perc
+print("ennyi perc lenne 26 órás napokkal egy héten:", ido)
